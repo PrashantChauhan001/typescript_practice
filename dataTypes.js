b251e3e1de0b352502ea56ba92bc2459c40ec737
Prashant Chauhan <prashant.chauhan@globalvoxinc.com>
2023-05-12 20:31:59 +0530

    initial commit

---
+++ dataTypes.js
@@ -0,0 +1,6 @@
+"use strict";
+// string
+let lName;
+lName = "Prashant";
+let newName = lName.toUpperCase();
+console.log(newName);
---
+++ dataTypes.ts
@@ -0,0 +1,9 @@
+// string
+
+let lName: String;
+
+lName = "Prashant";
+
+let newName = lName.toUpperCase();
+
+console.log(newName);
